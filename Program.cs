ec14fb2bf2c8dea93e140f51be348e12baf3f512
Jahanzeb Malik <jahanxeb.m@gmail.com>
2022-05-15 20:54:58 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Store user and computer inputs
+            string userInput, computerInput;
+            int randomInt;
+
+            Console.Write("Enter your choice - ROCK, PAPER or SCISSORS: ");
+
+            userInput = Console.ReadLine();
+
+            Random random = new Random();
+            randomInt = random.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    computerInput = "ROCK";
+                    break;
+                case 2:
+                    computerInput = "PAPER";
+                    break;
+                case 3:
+                    computerInput = "SCISSORS";
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+            }
+
         }
     }
 }
