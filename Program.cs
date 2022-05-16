e375cf5eb92e8e4867fd7e2a90135fa8c1dc8b45
Jahanzeb Malik <jahanxeb.m@gmail.com>
2022-05-16 13:58:49 +0100

    commit

--- Program.cs
+++ Program.cs
@@ -9,11 +9,14 @@ namespace RockPaperScissors
             //Store user and computer inputs
             string userInput, computerInput;
             int randomInt;
+            int userScore = 0;
+            int computerScore = 0;
 
             Console.Write("Enter your choice - ROCK, PAPER or SCISSORS: ");
 
             userInput = Console.ReadLine();
 
+            //Generate random number
             Random random = new Random();
             randomInt = random.Next(1, 4);
 
@@ -21,18 +24,47 @@ namespace RockPaperScissors
             {
                 case 1:
                     computerInput = "ROCK";
+                    Console.WriteLine("Computer chose {0}", computerInput);
+                    if (userInput == "ROCK")
+                    {
+                        Console.WriteLine("DRAW !!\n\n");
+                    }
+                    else if (userInput == "PAPER")
+                    {
+                        Console.WriteLine("PLAYER WINS !!\n\n");
+                        userScore++;
+                    }
+                    else if (userInput == "SCISSORS")
+                    {
+                        Console.WriteLine("COMPUTER WINS !!\n\n");
+                        computerScore++;
+                    }
                     break;
                 case 2:
                     computerInput = "PAPER";
+                    Console.WriteLine("Computer chose {0}", computerInput);
+                    if (userInput == "PAPER")
+                    {
+                        Console.WriteLine("DRAW !!\n\n");
+                    }
                     break;
                 case 3:
                     computerInput = "SCISSORS";
+                    Console.WriteLine("Computer chose {0}", computerInput);
                     break;
                 default:
                     Console.WriteLine("Invalid Entry");
                     break;
             }
-
+            //     public void gameRules()
+            //     {
+            //         if (userInput == "ROCK" && computerInput == "PAPER")
+            //         {
+            //             Console.WriteLine("PLAYER WINS!!!");
+            //             userScore++;
+            //         }
+            //         else if (userInput == "ROCK")
+            // }
         }
     }
 }
